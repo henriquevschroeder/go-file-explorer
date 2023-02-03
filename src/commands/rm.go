2273c9adfacd4dc445d37815b9fe0b3452a75823
Henrique Valiati Schroeder <henriquevschdev@gmail.com>
2023-02-02 23:11:34 -0300

    💥 fix: merge changes from HEAD

--- src/commands/rm.go
+++ src/commands/rm.go
@@ -5,28 +5,16 @@ import (
 	"os"
 )
 
-<<<<<<< HEAD
 func Rm(fileNames []string) {
 	for _, fileName := range fileNames {
 		err := os.Remove(fileName)
-	
+
 		if err != nil {
 			fmt.Printf("Error removing file '%s': %s", fileName, err)
 			os.Exit(1)
 		}
-	
+
 		fmt.Println("Removed file:", fileName)
 	}
 
-=======
-func Rm(filename string) {
-	err := os.Remove(filename)
-
-	if err != nil {
-		fmt.Println("Error removing file:", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Removed file:", filename)
->>>>>>> f797011b5fa32a1fbd65fdbb79aa2754767c4680
 }
--- src/commands/rmdir.go
+++ src/commands/rmdir.go
@@ -5,27 +5,15 @@ import (
 	"os"
 )
 
-<<<<<<< HEAD
 func Rmdir(dirNames []string) {
 	for _, dirName := range dirNames {
 		err := os.RemoveAll(dirName)
-	
+
 		if err != nil {
 			fmt.Printf("Error removing directory '%s': %s", dirName, err)
 			os.Exit(1)
 		}
-	
+
 		fmt.Println("Removed directory:", dirName)
 	}
-=======
-func Rmdir(dirname string) {
-	err := os.RemoveAll(dirname)
-
-	if err != nil {
-		fmt.Println("Error removing directory:", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Removed directory:", dirname)
->>>>>>> f797011b5fa32a1fbd65fdbb79aa2754767c4680
 }
